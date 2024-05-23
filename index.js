679668a90382806a5dbb8fbf3bb1c339a8018363
HAYBISH10 <hassanissackmohamed172@gmail.com>
2024-05-23 13:55:03 +0300

    snake master game

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ document.addEventListener("keydown", function(e){
         console.log(leftint)
             if(leftint >=0){
                 dorger.style.left = `${leftint-1}px`
-
-            }
+             }
             else{
                 alert("game over")
             }
@@ -20,10 +19,33 @@ document.addEventListener("keydown", function(e){
         if(leftint <=360){
             dorger.style.left = `${leftint+1}px`
 
+        }else{
+            alert("game over")
+        }
+   
+
+    }else if(e.key==="ArrowUp"){
+        const newup=dorger.style.bottom.replace("px", "")
+        const bottomint=parseInt(newup);
+        if(bottomint<=380){
+            dorger.style.bottom = `${bottomint + 1}px`
+        }else{
+            alert("game over")
         }
 
-    }else{
-        alert("Click the right key")
+
+    }else if(e.key==="ArrowDown"){
+        const newdown=dorger.style.bottom.replace("px", "")
+        const bottomint=parseInt(newdown);
+        if(bottomint>=0){
+            dorger.style.bottom = `${bottomint - 1}px`
+        }else{
+            alert("game over")
     }
     
+    
+}
+    else{
+        alert("Click the right key")
+    }
  })
